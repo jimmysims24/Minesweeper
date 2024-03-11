22c931fb3197de048062b9bda3f3cf45c82d8cbd
jimmysims24 <162385175+jimmysims24@users.noreply.github.com>
2024-03-11 16:50:04 +1300

    made changes to task1.py
    updated the position list

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 from functions_minesweeper import *
 
 
-positions =  [ [2, 3], [1, 4] ]  # Example positions of mines
+positions =  [ [2, 3], [1, 4], [3, 4], [4, 0], [0,3] ]  # Example positions of mines
 play_game(positions)
