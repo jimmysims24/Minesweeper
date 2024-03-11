5e0097e3c0dfad5241eefb31e95b6b398ff73f42
jimmysims24 <162385175+jimmysims24@users.noreply.github.com>
2024-03-11 16:47:00 +1300

    made changes to test_minesweeper.py
    Had to make changes

--- test_minesweeper.py
+++ test_minesweeper.py
@@ -1,12 +1,53 @@
 import pytest
 from functions_minesweeper import *
-
 def test_insert_mines():
-    pass
+    board = [
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O'
+    ]
+    positions = [ [2, 3], [1, 4] ]
+    updated_board = insert_mines(board, positions)
+    assert(updated_board == [
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'X',
+        'O', 'O', 'O', 'X', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O'
+    ])
+
 def test_count_adjacent_mines_in_corner():
-    pass
+    board = [
+        'O', 'O', 'O', 'O', 'X',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O'
+    ]
+    counts = count_adjacent_mines(board, 0, 3)
+    assert (counts == 1)
+
 def test_play_turn():
-    pass
-def test_check_win():
-    pass
+    board = [
+        'X', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+    ]
+    updated_board, mine_selected = play_turn(board, 1, 1)
+
+    assert updated_board != "Game Over"
+    assert isinstance(mine_selected, bool)
+def test_check_win_when_all_mines_selected():
+    board = [
+        'X', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+        'O', 'O', 'O', 'O', 'O',
+    ]
+    assert not check_win(board)
 
